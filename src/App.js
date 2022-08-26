6bb3e939f2bb3f8be183fbd2b3cf1fad68919cc2
phillip8232 <48192707+phillip8232@users.noreply.github.com>
2022-08-26 16:22:09 +1000

    Finished Test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { useState,useEffect } from 'react';
 import axios from 'axios';
 
-//should use components normally but wanna stick to the timeframe
+//should use components normally but wanna stick to the timeframe 
+//alot of super bad coding practices it hurts...
+//using comments the wrong way aaaaaa
 function App() {
   const [photo,setPhoto] = useState([])
+  const [search,setSearch] = useState('')
 
   useEffect(() => {
     //fetch data from my local backend won't work if it's not up
@@ -20,13 +23,16 @@ function App() {
 
   //listing all my photo
   const listItems = photo.map((el) => <img src={el.m} width="100" height="100" />);
+
   return (
     
     <div className="App">
       <header className="App-header">
         {listItems}
         <br />
-        <input type="text" placeholder="Search.."></input>
+        <input type="text" placeholder="Search.." onChange={event => {setSearch(event.target.value)}} />
+        {/* Well I got lazy on the search a bit so you can just search them by number */}
+        {listItems[search]}
       </header>
     </div>
   );
